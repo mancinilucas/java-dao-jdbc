0ddccc30fc3ef59ebf2b6012fdff3d5a1db88b56
mancinilucas <mancini.lucasg@gmail.com>
2023-01-26 20:04:25 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -11,7 +13,8 @@ public class Program {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(21, "Musa", "musa@gmail.com", new Date(), 3000.0, obj);
 
-        System.out.println(obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
 
     }
